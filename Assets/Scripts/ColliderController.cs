9678e478260d33dd232f985c0e32bc2d9664daff
Sebastian Kobori <sjkobori@gmail.com>
2022-02-20 14:12:17 -0500

    Basic Platformer controls

---
+++ Assets/Scripts/ColliderController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderController : MonoBehaviour
+{
+    public bool isColliding = false;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        isColliding = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isColliding = false;
+    }
+}
---
+++ Assets/Scripts/PlatformerPlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerPlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool jumping = false;
+    public float jumpPower = 5f;
+    public float moveSpeed = 5f;
+    public float acceleration = 1;
+    public float maxSpeed = 10;
+    
+    private ColliderController feet;
+    void Awake()
+    {
+        rb = GetComponentInChildren<Rigidbody2D>();
+        feet = GetComponentInChildren<ColliderController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInputs();
+    }
+
+    void GetInputs()
+    {
+        float xDir = Input.GetAxis("Horizontal");
+        if (feet.isColliding && Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        if (/*Mathf.Abs(rb.velocity.x) < maxSpeed &&*/ Mathf.Abs(xDir) > Mathf.Epsilon)
+        {
+            Move(xDir);
+        }
+    }
+    void Jump()
+    {
+        rb.velocity = new  Vector2 (rb.velocity.x, jumpPower);
+
+    }
+
+    private void Move(float xDir)
+    {
+        //rb.
+        rb.velocity = new Vector2(xDir * 10, rb.velocity.y);
+        //rb.AddForce(Vector2.right * acceleration * moveSpeed * Time.deltaTime * 10);
+    }
+
+
+}
