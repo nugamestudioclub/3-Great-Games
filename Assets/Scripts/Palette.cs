b9685d69a26acf0384c1920a13d3b3bdf8a0e718
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-02-22 11:37:14 -0500

    updated palette to be enumerable & read only list

--- Assets/Scripts/Palette.cs
+++ Assets/Scripts/Palette.cs
@@ -2,35 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Palette<T> : ScriptableObject
-{
-    [SerializeField]
-    private List<T> items;
+public abstract class Palette<T> : ScriptableObject, IEnumerable<T>, IReadOnlyList<T> {
+	[SerializeField]
+	private List<T> items;
+	public int Count => items.Count;
 
-    public T this[int index]
-    {
-        get => items[index % items.Count];
+	public T this[int index] {
+		get => items[index % items.Count];
+		set => items[index % items.Count] = value;
+	}
 
-        set => items[index] = value;
-    }
-    
-   
+	public IEnumerator<T> GetEnumerator() {
+		return ((IEnumerable<T>)items).GetEnumerator();
+	}
+
+	IEnumerator IEnumerable.GetEnumerator() {
+		return ((IEnumerable)items).GetEnumerator();
+	}
 }
 
-[CreateAssetMenu(fileName = "ColorPalette", menuName = "ScriptableObjects/Palette/Color", order = 1)] 
-public class ColorPalette : Palette<Color>
-{
+[CreateAssetMenu(fileName = "ColorPalette", menuName = "ScriptableObjects/Palette/Color", order = 1)]
+public class ColorPalette : Palette<Color> {
 
 }
 
 [CreateAssetMenu(fileName = "AudioPalette", menuName = "ScriptableObjects/Palette/Audio", order = 1)]
-public class AudioPalette : Palette<AudioClip>
-{
+public class AudioPalette : Palette<AudioClip> {
 
 }
 
 [CreateAssetMenu(fileName = "GameObjectPalette", menuName = "ScriptableObjects/Palette/GameObject", order = 1)]
-public class GameObjectPalette : Palette<GameObject>
-{
+public class GameObjectPalette : Palette<GameObject> {
 
 }
