38a0a9b18f9001c0bcf09b0e0e4c7a8af704a8c5
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-02-22 12:06:13 -0500

    added spawner

---
+++ Assets/Scripts/Platformer/PlatformerSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public enum PlatformerObjectType {
+	Coin,
+	Brick,
+	Goomba
+}
+
+public class PlatformerSpawner : Spawner {
+	[SerializeField]
+	private PlatformerObjectType type;
+
+	protected override int TypeId => (int)type;
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public abstract class Spawner : MonoBehaviour {
+	protected abstract int TypeId { get; }
+
+	void Start() {
+		Instantiate(
+			MinigameController.Instance.GameObject(TypeId),
+			transform.position,
+			transform.rotation
+		);
+	}
+}
