b81c19c31fe59e85969034cfe4e800d9aaa5136d
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-02-21 19:47:04 -0500

    added sprite tinter

---
+++ Assets/Scripts/GlitchySprite.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class GlitchySprite : MonoBehaviour {
+	[SerializeField]
+	private Color color;
+
+	private SpriteRenderer spriteRenderer;
+	private Sprite mainSprite;
+	private Sprite greySprite;
+
+	private static string GreyPath(string path) {
+		int pos = path.LastIndexOf('.');
+
+		return path.Substring(0, pos) + "_grey" + path.Substring(pos, path.Length - pos);
+	}
+
+	void Awake() {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		mainSprite = spriteRenderer.sprite;
+		greySprite = AssetDatabase.LoadAssetAtPath<Sprite>(GreyPath(AssetDatabase.GetAssetPath(mainSprite)));
+		spriteRenderer.sprite = greySprite;
+	}
+
+	public void Tint(Color color) {
+		if( color == this.color ) {
+			spriteRenderer.sprite = mainSprite;
+			spriteRenderer.color = Color.white;
+		}
+		else {
+			spriteRenderer.sprite = greySprite;
+			spriteRenderer.color = color;
+		}
+	}
+}
