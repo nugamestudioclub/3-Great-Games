a15dd47ae4c0e50eac4cdc01a72760bb39cfe1e2
Joe Turco <jaturco00@gmail.com>
2022-02-23 17:43:48 -0500

    Minor work on tank game

---
+++ Assets/Scripts/Tanks/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float rotSpeed;
+
+    [SerializeField]
+    private bool enable;
+
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enable)
+        {
+            Vector3 mov;
+            transform.Rotate(0, 0, Input.GetAxis("Horizontal") * rotSpeed * Time.deltaTime);
+            Debug.Log(transform.);
+            mov = new Vector3(speed * Mathf.Cos(transform.rotation.z / 360 * 2 * Mathf.PI) * Input.GetAxis("Vertical") * Time.deltaTime, 
+                speed * Mathf.Sin(transform.rotation.z / 360 * 2 * Mathf.PI) * Input.GetAxis("Vertical") * Time.deltaTime, 0);
+            controller.Move(mov);
+
+        }
+    }
+}
