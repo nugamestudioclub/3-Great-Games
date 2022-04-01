e298d60c762a355ded150e5b3cf41d2cdf3a3d07
Sebastian Kobori <sjkobori@gmail.com>
2022-04-01 09:16:02 -0400

    Animation assets added

--- Assets/Scripts/GameMemory.cs
+++ Assets/Scripts/GameMemory.cs
@@ -64,9 +64,9 @@ public class GameMemory : MonoBehaviour {
 
 		//for (int i = 0; i < ActiveCartridge.EntitiesPalette.Count; ++i)
 		//	memory[i + 2] = ActiveCartridge.EntitiesPalette[i];
-
+		
 		ApplyCorruption(Corruption % 5 + 1);
-
+		Refresh();
 		loaded = true;
 	}
 
