cb95f5f88b33b2c6a0f247c3df5ab5b6cb149f0d
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-04-23 18:31:54 -0400

    cleaned up menu navigation in anticipation of adding features

--- Assets/Scripts/MenuNavigation.cs
+++ Assets/Scripts/MenuNavigation.cs
@@ -3,70 +3,77 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MenuNavigation : MonoBehaviour
-{
-    [SerializeField]
-    private List<Button> options = new List<Button>();
+public class MenuNavigation : MonoBehaviour {
+	[SerializeField]
+	private List<Button> buttons = new List<Button>();
 
-    //hitting up input or or down input
-    [ReadOnly]
-    [SerializeField]
-    private int index;
+	[ReadOnly]
+	[SerializeField]
+	private int index;
 
-    private void Awake()
-    {
-        index = 0;
-        Select();
-    }
+	private void Awake() {
+		index = 0;
+		Select();
+	}
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            ExecuteOrder66();
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
-        {
-            Next();
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
-        {
-            Previous();
-        }
-    }
+	[SerializeField]
+	private List<KeyCode> clickKeys = new List<KeyCode>();
 
-    private void Select()
-    {
-        options[index].Select();
-    }
+	[SerializeField]
+	private List<KeyCode> previousKeys = new List<KeyCode>();
 
-    private void ExecuteOrder66()
-    {
-        options[index].onClick.Invoke();
-    }
-    /*
-    private void Unselect()
-    {
-        options[index].Des();
-    }
-    */
+	[SerializeField]
+	private List<KeyCode> nextKeys = new List<KeyCode>();
 
+	private void Update() {
+		if( GetAnyDown(clickKeys) ) {
+			Click();
+		}
+		else if( GetAnyDown(previousKeys) ) {
+			Previous();
+		}
+		else if( GetAnyDown(nextKeys) ) {
+			Next();
+		}
+	}
 
-    public void Previous()
-    {
-        Select();
-        --index;
-        if (index < 0) index = options.Count - 1;
-        Select();
+	private void Select() {
+		buttons[index].Select();
+	}
 
-    }
+	private void Click() {
+		buttons[index].onClick.Invoke();
+	}
 
-    public void Next()
-    {
-        Select();
-        ++index;
-        if (index >= options.Count) index = 0;
-        Select();
-    }
+	public void Previous() {
+		index = (--index + buttons.Count) % buttons.Count;
+		Select();
 
+	}
+
+	public void Next() {
+		index = ++index % buttons.Count;
+		Select();
+	}
+
+	public static bool GetAnyDown(IList<KeyCode> keys) {
+		foreach( var key in keys )
+			if( Input.GetKeyDown(key) )
+				return true;
+		return false;
+	}
+
+	public static bool GetAny(IList<KeyCode> keys) {
+		foreach( var key in keys )
+			if( Input.GetKey(key) )
+				return true;
+		return false;
+	}
+
+	public static bool GetAnyUp(IList<KeyCode> keys) {
+		foreach( var key in keys )
+			if( Input.GetKeyUp(key) )
+				return true;
+		return false;
+	}
 }
