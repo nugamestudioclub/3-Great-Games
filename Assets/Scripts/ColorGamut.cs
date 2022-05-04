dd9561f4e23ad57869b1819302d9b9fa6d8c4555
BBBrassil <56983102+BBBrassil@users.noreply.github.com>
2022-05-03 20:57:13 -0400

    added NES color gamut

---
+++ Assets/Scripts/ColorGamut.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(
+	fileName = nameof(ColorGamut),
+	menuName = Paths.SCRIPTABLE_OBJECTS + "/" + nameof(ColorGamut))
+]
+public class ColorGamut : ScriptableObject {
+	const int LUMA_COUNT = 4;
+
+	[SerializeField]
+	private Palette<Color> VeryDark;
+
+	[SerializeField]
+	private Palette<Color> Dark;
+
+	[SerializeField]
+	private Palette<Color> Medium;
+
+	[SerializeField]
+	private Palette<Color> Light;
+
+	public Color Color(GlitchyHue hue, GlitchyLuma luma) => Color((int)hue, (int)luma);
+
+	public Color Color(int hue, int luma) => (luma % LUMA_COUNT) switch {
+		(int)GlitchyLuma.VeryDark => VeryDark[hue],
+		(int)GlitchyLuma.Dark => Dark[hue],
+		(int)GlitchyLuma.Medium => Medium[hue],
+		(int)GlitchyLuma.Light => Light[hue],
+		_ => VeryDark[(int)GlitchyHue.Black]
+	};
+}
--- Assets/Scripts/GlitchyColor.cs
+++ Assets/Scripts/GlitchyColor.cs
@@ -3,36 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum GlitchyHue
-{
-    White,
-    Blue,
-    Indigo,
-    Violet,
-    Purple,
-    Pink,
-    Orange,
-    Amber,
-    Yellow,
-    LimeGreen,
-    Green,
-    Teal,
-    Cyan,
-    Black,
+public enum GlitchyHue {
+	White,
+	Blue,
+	Indigo,
+	Violet,
+	Purple,
+	Pink,
+	Orange,
+	Amber,
+	Yellow,
+	Lime,
+	Green,
+	Jade,
+	Cyan,
+	Black,
 }
 
-public enum GlitchyLuma
-{
-    VeryDark,
-    Dark,
-    Medium,
-    Light,
+public enum GlitchyLuma {
+	VeryDark,
+	Dark,
+	Medium,
+	Light,
 }
 
 [Serializable]
-public struct GlitchyColor
-{
-    public GlitchyHue hue;
-    public GlitchyLuma luma;
+public struct GlitchyColor {
+	public GlitchyHue hue;
+	public GlitchyLuma luma;
 }
 
